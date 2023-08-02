663e0c5e752f0583554365f8a8ec713535ca1416
Sushmitamaha27 <sushmitagmahajan@gmail.com>
2023-08-02 20:03:46 +0530

    created views and urls

---
+++ elevator_system/elevator/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ElevatorViewSet, ElevatorRequestViewSet
+
+
+
+router = DefaultRouter()
+router.register(r'elevators', ElevatorViewSet)
+router.register(r'requests', ElevatorRequestViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- elevator_system/elevator/views.py
+++ elevator_system/elevator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Elevator, ElevatorRequest
+
 
 # Create your views here.
+class ElevatorViewSet(viewsets.ModelViewSet):
+    pass
+
+
+class ElevatorRequestViewSet(viewsets.ModelViewSet):
+    pass
